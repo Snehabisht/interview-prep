1973f144a6b50c7f711e3c4ef27c5bd7ad37c06f
snehabisht <sneha.bisht12699@gmail.com>
2026-08-04 19:10:42 +0530

    virtual and pure virtual functions, abstract classes

--- cpp/abstraction/AbstractClasses.cpp
+++ cpp/abstraction/AbstractClasses.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Shape {
     public:
     virtual void draw() = 0; // pure virtual function
-    void display() {
+    void display() { // concrete function
         cout<<"displaying shape\n";
     }
 };
@@ -17,10 +17,27 @@ class Circle : public Shape {
     }
 };
 
+class Rectangle : public Shape {
+
+    public : 
+    void draw() {
+        cout<<"drawing a rectangle\n";
+    }
+};
+
 int main() {
-    Circle c1;
-    c1.draw();
-    c1.display();
-    return 0;
+   // Shape* c1 = new Rectangle(); we must implement the pure virtual function draw() in Rectangle class otherwise we cannot create an object of Rectangle class
+   Shape* c1 = new Circle();
+//    c1->draw();
+//    c1->display();
+   Shape* c2 = new Rectangle();
+
+   Shape* shapes[2] = {c1, c2}; //runtime polymorphism
+
+   for(int i = 0; i < 2; i++) {
+       shapes[i]->draw();
+   }
+
+   return 0;
 }
 
