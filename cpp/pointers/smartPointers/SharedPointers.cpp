ac2a83af6b0dd2f5ba7d9ba7b469e37f6c60e80a
snehabisht <sneha.bisht12699@gmail.com>
2026-08-16 17:41:09 +0530

    remaining changes

--- cpp/pointers/smartPointers/SharedPointers.cpp
+++ cpp/pointers/smartPointers/SharedPointers.cpp
@@ -15,7 +15,10 @@ class MyClass {
 int main() {
     shared_ptr<MyClass> sharedPtr = make_shared<MyClass>();
     cout<<" Shared count : "<<sharedPtr.use_count()<<endl; // returns the number of shared_ptr instances managing the same object
-    shared_ptr<MyClass> sharedPtr2 = sharedPtr; // shared ownership of the same object 
+    {
+        shared_ptr<MyClass> sharedPtr2 = sharedPtr; // shared ownership of the same object 
+        cout<<" Shared count : "<<sharedPtr.use_count()<<endl;
+    }
     cout<<" Shared count : "<<sharedPtr.use_count()<<endl;
     return 0;
 }
--- cpp/pointers/smartPointers/UniquePointers.cpp
+++ cpp/pointers/smartPointers/UniquePointers.cpp
@@ -4,7 +4,11 @@ using namespace std;
 
 int main() {
     unique_ptr<int> unPtr = make_unique<int>(25);
+    //*unPtr = 10; also applicable
     unique_ptr<int> unPtr2 = move(unPtr); // transferring ownership of unique_ptr to another unique_ptr
+    if(unPtr==NULL){
+        cout<<"unPtr now points to NULL\n";
+    }
     unique_ptr<int> unPtr3(new int);
     cout<<*unPtr2<<endl; // dereferencing unique_ptr to get the value
     // cout<<*unPtr<<endl; NULL // dereferencing unique_ptr after ownership has been transferred will result in undefined behavior
